b062b3ca240ab8363e80f9f7aa5c57520586ea94
oneyoung@ThinkC <guowangyang@gmail.com>
2012-09-24 16:57:13 +0800

    [vim][python]make sure cmd terminated when vim exit

--- .vim/python/stream.py
+++ .vim/python/stream.py
@@ -1,12 +1,19 @@
 import vim
 import subprocess
 from threading import Thread as thread
+import atexit
 
 def output_lines_to_buffer(cmd):
     """
     Append the given shell command's output linewise to the current buffer.
     """
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    @atexit.register
+    def exit_handler():
+        #if the cmd is not exit yet when vim exit, terminate it
+        if p.poll() is None:
+            p.terminate()
+
     vim_add = vim.current.buffer.append
     vim_cmd = vim.command
     for line in iter(p.stdout.readline, ''):
