1d44fbb056a11b721fd18c0b9c41212b266c4ffe
oneyoung@ThinkC <guowangyang@gmail.com>
2012-09-24 16:15:32 +0800

    [vim]add python script to edit cmd output stream

---
+++ .vim/python/stream.py
@@ -0,0 +1,19 @@
+import vim
+import subprocess
+from threading import Thread as thread
+
+def output_lines_to_buffer(cmd):
+    """
+    Append the given shell command's output linewise to the current buffer.
+    """
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    vim_add = vim.current.buffer.append
+    vim_cmd = vim.command
+    for line in iter(p.stdout.readline, ''):
+        vim_add(line)
+        vim_cmd('redraw')
+
+def run(cmd):
+    th = thread(target = output_lines_to_buffer, args = ([cmd]))
+    th.daemon = True
+    th.start()
