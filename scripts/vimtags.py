9d30440dc9ac5cdcd00ea09c66169ed6b68a971d
oneyoung@ThinkC <guowangyang@gmail.com>
2014-03-07 17:15:50 +0800

    [script] vimtags.sh ==> vimtags.py

---
+++ scripts/vimtags.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+
+
+db = 'cscope.file'
+
+if not os.path.exists(db):
+    os.system('''find . -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.S" -o -name "*.mk" -o -name Makefile -o -name "*.lds" -o -name "*.inc" -o -name "*.make" >  %s ''' % db)
+
+os.system("ctags -L %s" % db)
+os.system("cscope -bkq -i %s" % db)
