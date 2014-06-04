9dd91c956cc4706fa000b7de810d3ab7287e6090
oneyoung@ThinkC <guowangyang@gmail.com>
2014-06-04 13:18:41 +0800

    [scripts] more files for vimtags.py

--- scripts/vimtags.py
+++ scripts/vimtags.py
@@ -5,7 +5,7 @@ import os
 db = 'cscope.file'
 
 if not os.path.exists(db):
-    os.system('''find . -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.S" -o -name "*.mk" -o -name Makefile -o -name "*.lds" -o -name "*.inc" -o -name "*.make" >  %s ''' % db)
+    os.system('''find . -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.S" -o -name "*.mk" -o -name Makefile -o -name "*.lds" -o -name "*.inc" -o -name "*.make" -o -name "*.java" >  %s ''' % db)
 
 os.system("ctags -L %s" % db)
 os.system("cscope -bkq -i %s" % db)
